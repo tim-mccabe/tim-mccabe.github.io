0efa36dc60d9073c1e28b7dcc7ffecb5d6112add
Tim McCabe <timmccabe44@gmail.com>
2020-05-20 12:08:52 -0400

    update

---
+++ js/scatter_plot.js
@@ -0,0 +1,213 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3.select(".chart");
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "Year_Built";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(golfData, chosenXAxis) {
+    // create scales
+    var xLinearScale = d3.scaleLinear()
+      .domain([d3.min(golfData, d => d[chosenXAxis]) * 0.995,
+        d3.max(golfData, d => d[chosenXAxis]) * 1.02
+      ])
+      .range([0, width]);
+  
+    return xLinearScale;
+  
+}
+
+// function used for updating xAxis var upon click on axis label
+function renderAxes(newXScale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXScale);
+  
+    xAxis.transition()
+      .duration(1000)
+      .call(bottomAxis);
+  
+    return xAxis;
+}
+
+// function used for updating circles group with a transition to
+// new circles
+function renderCircles(circlesGroup, newXScale, chosenXaxis) {
+
+    circlesGroup.transition()
+      .duration(1000)
+      .attr("cx", d => newXScale(d[chosenXAxis]));
+  
+    return circlesGroup;
+}
+
+// function used for updating circles group with new tooltip
+function updateToolTip(chosenXAxis, circlesGroup) {
+
+    if (chosenXAxis === "Year_Built") {
+      var label = "Year Built:";
+    }
+    else {
+      var label = "Length of Course (yards):";
+    }
+  
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(function(d) {
+        return (`${d.Course_Name}<br>${label} ${d[chosenXAxis]}`);
+      });
+  
+    circlesGroup.call(toolTip);
+  
+    circlesGroup.on("mouseover", function(data) {
+      toolTip.show(data);
+    })
+      // onmouseout event
+      .on("mouseout", function(data) {
+        toolTip.hide(data);
+      });
+  
+    return circlesGroup;
+}
+
+// Retrieve data from the CSV file and execute everything below
+d3.csv("course_info.csv").then(function(golfData, err) {
+    if (err) throw err;
+  
+    // parse data
+    golfData.forEach(function(data) {
+      data.Rank = +data.Rank;
+      data.Year_Built = +data.Year_Built;
+      data.Yards = +data.Yards;
+    });
+
+  // xLinearScale function above csv import
+  var xLinearScale = xScale(golfData, chosenXAxis);
+
+  // Create y scale function
+  var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(golfData, d => d.Rank)])
+    .range([height, 0]);
+
+  // Create initial axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale); 
+  
+  // append x axis
+  var xAxis = chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  // append y axis
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // append initial circles
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(golfData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d[chosenXAxis]))
+    .attr("cy", d => yLinearScale(d.Rank))
+    .attr("r", 7)
+    .attr("fill", "green")
+    .attr("opacity", ".75");
+
+  // Create group for  2 x- axis labels
+  var labelsGroup = chartGroup.append("g")
+    .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+    var yearBuiltLabel = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 20)
+    .attr("value", "Year_Built") // value to grab for event listener
+    .classed("active", true)
+    .text("Year Built");
+
+    var yardsLabel = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 40)
+    .attr("value", "Yards") // value to grab for event listener
+    .classed("inactive", true)
+    .text("Length of Course (yards)");
+
+  // append y axis
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .classed("axis-text", true)
+    .text("Rank");
+
+  // updateToolTip function above csv import
+  var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+
+ // x axis labels event listener
+ labelsGroup.selectAll("text")
+ .on("click", function() {
+   // get value of selection
+   var value = d3.select(this).attr("value");
+   if (value !== chosenXAxis) {
+
+     // replaces chosenXAxis with value
+     chosenXAxis = value;
+
+     // console.log(chosenXAxis)
+
+     // functions here found above csv import
+     // updates x scale for new data
+     xLinearScale = xScale(golfData, chosenXAxis);
+
+     // updates x axis with transition
+     xAxis = renderAxes(xLinearScale, xAxis);
+
+     // updates circles with new x values
+     circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
+
+     // updates tooltips with new info
+     circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+
+     // changes classes to change bold text
+     if (chosenXAxis === "Year_Built") {
+       yearBuiltLabel
+         .classed("active", true)
+         .classed("inactive", false);
+       yardsLabel
+         .classed("active", false)
+         .classed("inactive", true);
+     }
+     else {
+       yearBuiltLabel
+         .classed("active", false)
+         .classed("inactive", true);
+       yardsLabel
+         .classed("active", true)
+         .classed("inactive", false);
+     }
+   }
+ });
+}).catch(function(error) {
+console.log(error);
+});
